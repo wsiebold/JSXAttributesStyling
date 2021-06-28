88ac054fbbafba5665ae57d6e9665f8bbae7b2a9
wsiebold <email@designpdx.com>
2021-06-28 11:26:40 -0700

    message and color based on currentTime - dynamically changing style using const / inline style

--- src/index.js
+++ src/index.js
@@ -1,34 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const img1 =
-  "https://www.gannett-cdn.com/presto/2020/05/14/USAT/cf6c4584-1b5d-4453-a949-7268d8726122-AP_Switzerland_Montreux_Jazz_Festival.JPG?width=660&height=440&fit=crop&format=pjpg&auto=webp";
-const img2 =
-  "https://media.stubhubstatic.com/stubhub-catalog/d_defaultLogo.jpg/t_f-fs-0fv,q_auto:low,f_auto,c_fill,$w_280_mul_3,$h_180_mul_3/performer/1507017/78fa126de4adb0bf3e59da48496cd104";
-const img3 =
-  "https://imengine.prod.srp.navigacloud.com/?uuid=5AFACCFC-239C-4D84-8AE2-B57069A71A8F&type=primary&q=72&width=1024";
+const date = new Date(2021, 6, 28, 13);
+const currentTime = date.getHours();
+const currentMin = date.getMinutes();
 
-const customStyle = {
-  color: "blue",
-  fontSize: "2em",
-  marginBottom: "2em"
+let greeting;
+
+const customColor = {
+  color: ""
 };
 
+if (currentTime < 12) {
+  greeting = "it is morning";
+  customColor.color = "red";
+} else if (currentTime < 18) {
+  greeting = "it is afternoon";
+  customColor.color = "green";
+} else {
+  greeting = "it is evening or night";
+  customColor.color = "blue";
+}
+
 ReactDOM.render(
-  <div>
-    <h1
-      style={customStyle}
-      className="heading"
-      contentEditable="true"
-      spellCheck="false"
-    >
-      Jethro Tull
-    </h1>
-    <div className="img-set" contentEditable="false">
-      <img alt="imgs in jsx" src={img1} />
-      <img alt="imgs in jsx" src={img2} />
-      <img alt="imgs in jsx" src={img3} />
-    </div>
+  <div className="heading">
+    <h1 style={customColor}>{greeting}</h1>
   </div>,
   document.getElementById("root")
 );
